57c9f4bda93acd2d7682d550d7200127038e283e
AKASHSAHA48 <33663773+AKASHSAHA48@users.noreply.github.com>
2019-11-13 20:01:41 +0600

    Add files via upload

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContosoSite.Controllers
+{
+    public class BaseController : Controller
+    {
+        // GET: Base
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if(Session["Username"]==null)
+            {
+                Response.Redirect("http://localhost:60977/Login");
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContosoSite.Models;
+
+namespace ContosoSite.Controllers
+{
+    public class LoginController : Controller
+    {
+        private ATP2Entities context = new ATP2Entities();
+        public bool Validate(Admin admin)
+        {
+            Admin valid = this.context.Admins.SingleOrDefault(u => u.UserName == admin.UserName && u.Password == admin.Password);
+            return valid != null;
+        }
+
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Admin admin)
+        {
+            bool valid = Validate(admin);
+            if (valid)
+            {
+                Session["Username"] = admin.UserName;
+                return RedirectToAction("Index", "Home");
+
+;            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
